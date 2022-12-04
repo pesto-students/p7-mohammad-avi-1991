6e94e894d7ddc0304ef1a1a682663d5f64770102
slow-learn-err <abpandey0791@gmail.com>
2022-12-04 20:05:12 +0530

    teach metthod creation

---
+++ Week-4/exercise4_2.js
@@ -0,0 +1,22 @@
+var Person = function(){};
+
+Person.prototype.initialize = function(name, age)
+{
+    this.name = name;
+    this.age = age;
+}
+
+var Teacher = function(){};
+
+Teacher.prototype.teach = function(subject)
+
+{
+    console.log(this.name +" is now teaching "+ subject);
+}
+
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
+
+
+var him = new Teacher();
+him.initialize("Adam", 45);
+him.teach("Inheritance");
